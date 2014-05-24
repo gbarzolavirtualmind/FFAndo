67b226513176b919585f507c426f92ba904b518c
recox <recox.elbostero@gmail.com>
2014-05-23 23:20:28 -0300

    Puestos en el home video y upload el view.on init

--- routes/views/home.js
+++ routes/views/home.js
@@ -4,7 +4,7 @@ var Home = keystone.list('Home');
 exports = module.exports = function(req, res) {
 	
 	var locals = res.locals,
-		view = new keystone.View(req, res);
+		  view = new keystone.View(req, res);
 
 	locals.formData = req.body || {};
     locals.validationErrors = {};
@@ -12,27 +12,55 @@ exports = module.exports = function(req, res) {
 	
 	// Set locals
 	locals.section = 'home';
+  locals.data = {
+    lastHashtags: [],
+    moreUsedHashtags: [],
+    outstandingVideos: [],
+    lastVideos: []
+  };
 	
 
-view.on('post', { action: 'contact' }, function(next) {
-               
-               var model = new Home.model(),
-                       updater = model.getUpdateHandler(req);
-               
-               updater.process(req.body, {
-                       flashErrors: true,
-                       //fields: 'name, email, phone, enquiryType, message',
-                       errorMessage: 'There was a problem submitting your enquiry:'
-               }, function(err) {
-                       if (err) {
-                               locals.validationErrors = err.errors;
-                       } else {
-                               locals.enquirySubmitted = true;
-                       }
-                       next();
-               });
-               
-       });
+  view.on('init', function(next) {
+    var q = keystone.list('Tag').model.find().limit('20');
+      q.exec(function(err, results) {
+        locals.data.lastHashtags = results;
+      });
+
+    var q = keystone.list('Video').model.find().limit('3');
+      q.exec(function(err, results) {
+        locals.data.outstandingVideos = results;
+      });
+
+    var q = keystone.list('Video').model.find().limit('6');
+      q.exec(function(err, results) {
+        locals.data.lastVideos = results;
+      });
+
+    var q = keystone.list('Video').model.find().limit('6');
+      q.exec(function(err, results) {
+        locals.data.moreUsedHashtags = results;
+        next(err);
+      });
+
+  });
+
+  view.on('post', { action: 'contact' }, function(next) {          
+     var model = new Home.model(),
+         updater = model.getUpdateHandler(req);
+     
+     updater.process(req.body, {
+             flashErrors: true,
+             //fields: 'name, email, phone, enquiryType, message',
+             errorMessage: 'There was a problem submitting your enquiry:'
+     }, function(err) {
+             if (err) {
+                     locals.validationErrors = err.errors;
+             } else {
+                     locals.enquirySubmitted = true;
+             }
+             next();
+     });      
+   });
 
 	// Render the view
 	view.render('home');
