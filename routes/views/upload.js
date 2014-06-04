4dc55d36195f822818016103827f25b6d5b64901
recox <recox.elbostero@gmail.com>
2014-06-04 00:46:27 -0300

    hecho algo con las vistas

--- routes/views/upload.js
+++ routes/views/upload.js
@@ -23,47 +23,23 @@ exports = module.exports = function(req, res) {
 
 view.on('post', { action: 'contact' }, function(next) {
                
-               var model = new Upload.model(),
-                       updater = model.getUpdateHandler(req);
-               
-               updater.process(req.body, {
-                       flashErrors: true,
-                       //fields: 'name, email, phone, enquiryType, message',
-                       errorMessage: 'There was a problem submitting your enquiry:'
-               }, function(err) {
-                       if (err) {
-                               locals.validationErrors = err.errors;
-                       } else {
-                               locals.enquirySubmitted = true;
-                       }
-                       next();
-               });
-               
-       });
-
-  view.on('init', function(next) {
-    var q = keystone.list('Tag').model.find().limit('20');
-      q.exec(function(err, results) {
-        locals.data.lastHashtags = results;
-      });
-
-    var q = keystone.list('Video').model.find().limit('3');
-      q.exec(function(err, results) {
-        locals.data.outstandingVideos = results;
-      });
-
-    var q = keystone.list('Video').model.find().limit('6');
-      q.exec(function(err, results) {
-        locals.data.lastVideos = results;
-      });
-
-    var q = keystone.list('Video').model.find().limit('6');
-      q.exec(function(err, results) {
-        locals.data.moreUsedHashtags = results;
-        next(err);
-      });
-
-  });
+   var model = new Upload.model(),
+           updater = model.getUpdateHandler(req);
+   
+   updater.process(req.body, {
+           flashErrors: true,
+           //fields: 'name, email, phone, enquiryType, message',
+           errorMessage: 'There was a problem submitting your enquiry:'
+   }, function(err) {
+           if (err) {
+                   locals.validationErrors = err.errors;
+           } else {
+                   locals.enquirySubmitted = true;
+           }
+           next();
+   });
+       
+});
 
 	// Render the view
 	view.render('upload');
